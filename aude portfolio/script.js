a0d9f7be214b68523efb68a6c3947879517fdcba
aude ritel-pignel <139326404+heresa12@users.noreply.github.com>
2023-10-19 14:44:14 +0200

    Add files via upload

--- aude portfolio/script.js
+++ aude portfolio/script.js
@@ -1,10 +1,11 @@
+// import LocomotiveScroll from './lib/locomotive-scroll.js';
+
 const scroll = new LocomotiveScroll({
   el: document.querySelector("[data-scroll-container]"),
   smooth: true,
   tablet: { smooth: true },
   smartphone: { smooth: true },
 });
-
 // Body color
 scroll.on("scroll", () => {
   if (document.querySelector("#color.is-inview")) {
